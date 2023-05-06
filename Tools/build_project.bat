e18194daf370310a60f4fd5e1663115dc00110fc
eXmatrx <matrx.eternal@gmail.com>
2023-05-06 19:10:01 +0200

    Update

--- Tools/build_project.bat
+++ Tools/build_project.bat
@@ -12,6 +12,7 @@ if NOT "%PROJECT%" == "client" (
    )
 )
 
+set PLATFORM=win
 set BUILD_ARCH=x64
 set DOTNET_VERSION=net7.0
 set CONFIGURATION=Debug
@@ -21,39 +22,44 @@ if NOT "%2" == "" (
    set CONFIGURATION=%2
 )
 
-REM Sets the architecture
+REM Sets the platform
 if NOT "%3" == "" (
-   set BUILD_ARCH=%3
+   set PLATFORM=%3
+)
+
+REM Sets the architecture
+if NOT "%4" == "" (
+   set BUILD_ARCH=%4
 )
 
 REM Sets the dotnet version
-if NOT "%4" == "" (
-   set DOTNET_VERSION=%4
+if NOT "%5" == "" (
+   set DOTNET_VERSION=%5
 )
 
 REM Builds the Code Preprocessor
-dotnet build ..\ReNote.CodeProcessor\ReNote.CodeProcessor.csproj --no-self-contained --runtime win-%BUILD_ARCH%
+dotnet build ..\ReNote.CodeProcessor\ReNote.CodeProcessor.csproj --no-self-contained --runtime %PLATFORM%-%BUILD_ARCH%
 
 REM Creates the processor directory
 mkdir processor
 
 REM Moves output of the Code Preprocessor to the Tools\processor directory
-call move_output.bat ..\ReNote.CodeProcessor\bin\%CONFIGURATION%\%DOTNET_VERSION%\win-%BUILD_ARCH% processor ReNote.CodeProcessor.exe codeproc.exe
+call move_output.bat ..\ReNote.CodeProcessor\bin\%CONFIGURATION%\%DOTNET_VERSION%\%PLATFORM%-%BUILD_ARCH% processor ReNote.CodeProcessor.exe codeproc.exe
 
 REM Runs the Code Preprocessor
 call codeprocessor.bat %PROJECT%
 
 REM Builds the Project
 if "%PROJECT%" == "client" (
-   dotnet build ..\Client\Client.csproj -c %CONFIGURATION% --no-self-contained --runtime win-%BUILD_ARCH%
+   dotnet build ..\Client\Client.csproj -c %CONFIGURATION% --no-self-contained --runtime %PLATFORM%-%BUILD_ARCH%
 ) else (
-   dotnet build ..\Server\Server.csproj -c %CONFIGURATION% --no-self-contained --runtime win-%BUILD_ARCH%
+   dotnet build ..\Server\Server.csproj -c %CONFIGURATION% --no-self-contained --runtime %PLATFORM%-%BUILD_ARCH%
 )
 
 goto end
 
 :usage
-echo Usage: build_project.bat ^<client ^| server^> [Configuration] [Architecture] [.NET Version]
+echo Usage: build_project.bat ^<client ^| server^> [Configuration] [Platform] [Architecture] [.NET Version]
 exit /b 1
 
 :end
--- Tools/build_project.sh
+++ Tools/build_project.sh
@@ -4,11 +4,12 @@ PROJECT=$1
 
 if { [[ $PROJECT == "" ]] || [[ $PROJECT != "client" ]]; } then
     if [[ $PROJECT != "server" ]]; then
-        echo "Usage: build_project.bat <client | server> [Configuration] [Architecture] [.NET Version]"
+        echo "Usage: build_project.bat <client | server> [Configuration] [Platform] [Architecture] [.NET Version]"
         exit 1
     fi
 fi
 
+PLATFORM=linux
 BUILD_ARCH=x64
 DOTNET_VERSION=net7.0
 CONFIGURATION=Debug
@@ -18,31 +19,36 @@ if [[ $2 != "" ]]; then
     CONFIGURATION=$2
 fi
 
-# Sets the architecture
+# Sets the platform
 if [[ $3 != "" ]]; then
-    BUILD_ARCH=$3
+    PLATFORM=$3
+fi
+
+# Sets the architecture
+if [[ $4 != "" ]]; then
+    BUILD_ARCH=$4
 fi
 
 # Sets the dotnet version
-if [[ $4 != "" ]]; then
-    DOTNET_VERSION=$4
+if [[ $5 != "" ]]; then
+    DOTNET_VERSION=$5
 fi
 
 # Builds the Code Preprocessor
-dotnet build ../ReNote.CodeProcessor/ReNote.CodeProcessor.csproj --no-self-contained --runtime linux-$BUILD_ARCH
+dotnet build ../ReNote.CodeProcessor/ReNote.CodeProcessor.csproj --no-self-contained --runtime $PLATFORM-$BUILD_ARCH
 
 # Creates the processor directory
 mkdir processor
 
 # Moves output of the Code Preprocessor to the Tools\processor directory
-./move_output.sh ../ReNote.CodeProcessor/bin/$CONFIGURATION/$DOTNET_VERSION/linux-$BUILD_ARCH ./processor ReNote.CodeProcessor codeproc
+./move_output.sh ../ReNote.CodeProcessor/bin/$CONFIGURATION/$DOTNET_VERSION/$PLATFORM-$BUILD_ARCH ./processor ReNote.CodeProcessor codeproc
 
 # Runs the Code Preprocessor
 ./codeprocessor.sh $PROJECT
 
 # Builds the Project
 if [[ $PROJECT == "client" ]]; then
-    dotnet build ../Client/Client.csproj -c $CONFIGURATION --no-self-contained --runtime linux-$BUILD_ARCH
+    dotnet build ../Client/Client.csproj -c $CONFIGURATION --no-self-contained --runtime $PLATFORM-$BUILD_ARCH
 else
-    dotnet build ../Server/Server.csproj -c $CONFIGURATION --no-self-contained --runtime linux-$BUILD_ARCH
+    dotnet build ../Server/Server.csproj -c $CONFIGURATION --no-self-contained --runtime $PLATFORM-$BUILD_ARCH
 fi
